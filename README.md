404f879329c34719141809ae947a872f001e6804
johnyslima <johnyslima@yandex.ru>
2019-09-15 23:50:52 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# test_template
-Верстка тестового макета
+# Description
+
+Верстка тестового макета.
+
+___
+
+### Installing
+
+1. `git clone https://github.com/johnyslima/test_template.git`
+2. `npm install`
+3. `gulp`
+
+___
+
+### Dependencies
+
+* `browser-sync`
+* `del`
+* `gulp`
+* `gulp-cache`
+* `gulp-cssnano`
+* `gulp-if`
+* `gulp-imagemin`
+* `gulp-sass`
+* `gulp-uglify`
+* `gulp-useref`
+* `gulp-autoprefixer`
+* `run-sequence`
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var rigger = require('gulp-rigger');
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+});
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 4 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(rigger())
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'));
+});
+
+// Compress all images and move them to /dist/images
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        // Caching images that ran through imagemin
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'));
+});
+
+// Moves all fonts into /dist folder
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+// gulp.task('icons', function() {
+//     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+//         .pipe(gulp.dest('dist/webfonts/'));
+// });
+
+// Gulp will delete the `dist` folder for you whenever gulp clean:dist is run.
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
+
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
+});
+
+gulp.task('default', function (callback) {
+    runSequence(['sass','browser-sync', 'watch'],
+        callback
+    )
+});
